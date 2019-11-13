fc8f3ac18a0309fa5768c34bc773bccaf0c76504
skye <skyesajous@gmail.com>
2019-11-12 23:57:12 -0800

    navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import "./App.css";
+// import { Home, About, Contact, Navigation } from "./components";
+import { Route } from "react-router-dom";
 
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Navigation from "./components/Navigation";
 const App = () => (
   <div>
     <Navigation />
+    <Route path="/" exact components={Home} />
+    <Route path="/about/" exact components={About} />
+    <Route path="/contact/" exact components={Contact} />
   </div>
 );
 
